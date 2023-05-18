7ff233d8b2e952c1a420a93657273cf3c2c25dcd
yyt <yyt@yyt.ca>
2023-05-17 21:01:50 -0400

    add seo shit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { setAxiosDefault } from 'utils/axios';
 
 setAxiosDefault();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Old Viral Videos</title>
+        <meta
+          name="description"
+          content="Take a trip down memory lane and watch some old viral videos"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
